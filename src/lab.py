7c241fa72ba8320d43487c0eaeaf1200e84fb873
hibana2077 <hibana2077@gmail.com>
2024-04-20 11:28:00 +0800

    Update train_setting.yaml with modified learning rate

--- src/lab.py
+++ src/lab.py
@@ -1,8 +1,8 @@
 '''
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-04-17 15:56:39
-LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2024-04-17 15:56:45
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2024-04-20 11:27:19
 FilePath: /2024_president/ml/lab.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -11,7 +11,6 @@ import pprint
 
 file_name = "./train_setting.yaml"
 
-with open(file_name, "r") as f:
-    data = yaml.safe_load(f)
+train_setting = yaml.safe_load(open("train_setting.yaml"))
 
-pprint.pprint(data)
+pprint.pprint(train_setting)
