484a7987f01bef16e88f38f7e138f2c63070443f
hibana2077 <hibana2077@gmail.com>
2024-04-21 08:53:49 +0800

    Update sft_trainer.py with modified LoRA configuration and method check

--- src/sft_trainer.py
+++ src/sft_trainer.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-04-17 15:26:22
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-04-20 23:29:20
+LastEditTime: 2024-04-21 08:53:43
 FilePath: /2024_president/ml/sft_train.py
 Description:
 '''
@@ -16,49 +16,61 @@ from accelerate import PartialState
 from warnings import filterwarnings
 
 filterwarnings("ignore")
-
+# Load train settings from YAML file
 train_setting = safe_load(open("train_setting.yaml"))
 
+# Get device string
 device_string = PartialState().process_index
 print(f"Device: {device_string}")
 
+# Load dataset
 dataset = load_dataset(train_setting['dataset']['name'], split=train_setting['dataset']['split'])
 
+# Load model
 model = AutoModelForCausalLM.from_pretrained(train_setting['model']['name'],
-                                             device_map={'':device_string})
+                                             device_map={'': device_string})
 
-peft_config = LoraConfig(
-    r=int(train_setting['lora_config']['r']),
-    lora_alpha=int(train_setting['lora_config']['lora_alpha']),
-    lora_dropout=float(train_setting['lora_config']['lora_dropout']),
-    bias=str(train_setting['lora_config']['bias']),
-    task_type=str(train_setting['lora_config']['task_type']),
-)
-model = get_peft_model(model, peft_config) if train_setting['fine_tuning']['method'] == 'lora' else model
-print("LoRA method can't be used in to mergekit. Please use the full-finetuning method.") if train_setting['fine_tuning']['method'] == 'lora' else None
-print(model)
+# Configure LoRA if fine-tuning method is 'lora'
+if train_setting['fine_tuning']['method'] == 'lora':
+    peft_config = LoraConfig(
+        r=int(train_setting['lora_config']['r']),
+        lora_alpha=int(train_setting['lora_config']['lora_alpha']),
+        lora_dropout=float(train_setting['lora_config']['lora_dropout']),
+        bias=str(train_setting['lora_config']['bias']),
+        task_type=str(train_setting['lora_config']['task_type']),
+    )
+    model = get_peft_model(model, peft_config)
+    print("LoRA method can't be used in to mergekit. Please use the full-finetuning method.")
+    print(model)
+else:
+    print(model)
 
-traine_args = TrainingArguments(
+# Configure training arguments
+training_args = TrainingArguments(
     output_dir=train_setting['training_args']['output_dir'],
     per_device_train_batch_size=int(train_setting['training_args']['per_device_train_batch_size']),
     gradient_accumulation_steps=int(train_setting['training_args']['gradient_accumulation_steps']),
     learning_rate=float(train_setting['training_args']['learning_rate']),
-    logging_steps = int(train_setting['training_args']['logging_steps']),
+    logging_steps=int(train_setting['training_args']['logging_steps']),
 )
 
+# Create SFTTrainer instance
 trainer = SFTTrainer(
     model,
     train_dataset=dataset,
     dataset_text_field=train_setting['trainer']['dataset_text_field'],
     max_seq_length=int(train_setting['trainer']['max_seq_length']),
-    args=traine_args
+    args=training_args
 )
-# we don't pass the tokenizer to the trainer, because it will automatically use original tokenizer of the model
+# We don't pass the tokenizer to the trainer, because it will automatically use the original tokenizer of the model
 
+# Start training
 print("Start training")
 trainer.train()
 
+# Save model
 print("Save model")
 trainer.save_model()
 
+# Finish training
 print("Finish training")
